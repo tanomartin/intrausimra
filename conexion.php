443b9bcc0ed6db5dfc274c4dc05a6ad086665ae4
tanomartin05 <tanomartin05@gmail.com>
2014-03-07 12:38:49 +0000

    conexion sin contra.

--- conexion.php
+++ conexion.php
@@ -1,7 +1,7 @@
 <?php
 $host = "localhost";
-$user = "ospimrem_charly";
-$pass = "arce4651";
+$user = "XXXXX";
+$pass = "XXXXX";
 $db = mysql_connect($host,$user,$pass);
 mysql_select_db('ospimrem_intranet');
 $delcod = $_SESSION['delcod'];
