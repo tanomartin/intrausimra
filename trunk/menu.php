60adf6423cfcc0f931f8a45c7211eb7b88a48c1e
tanomartin05@gmail.com <tanomartin05@gmail.com@d5d51db0-0a2c-3ee3-4a00-c78fad0a9f02>
2014-06-16 20:52:39 +0000

    cambio fecha actualizacion

--- trunk/menu.php
+++ trunk/menu.php
@@ -52,7 +52,7 @@ $row=mysql_fetch_array($result);
 <table width="100%" border="0" align="center">
   <tr>
     <td height="33">&nbsp;</td>
-    <td colspan="2" align="right" class="Estilo3"><div align="center" class="Estilo13">ULTIMA ACTUALIZACI&Oacute;N - 06/03/2014 </div></td>
+    <td colspan="2" align="right" class="Estilo3"><div align="center" class="Estilo13">ULTIMA ACTUALIZACI&Oacute;N - 16/06/2014 </div></td>
     <td>&nbsp;</td>
   </tr>
   <tr>
